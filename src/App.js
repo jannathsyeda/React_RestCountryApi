ae12e0149ae32a0447a18b21929b419f8ec6d979
unknown <jannathsyeda7@gmail.com>
2021-04-01 19:35:01 +0600

    add country components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React,{useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Cart from './components/Cart/Cart';
+import Country from './components/Country/Country'
 
 
 
 function App() {
-  const [country, setCountry]= useState([]);
-  const useEffect(
-    
-    ,[])
+  const [countries, setCountry]= useState([]);
+  const [cart, setCart]=useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res =>res.json())
+    .then(data=>setCountry(data));
+  },[])
+
+  const handleAddCountry=(country)=>{
+    const newCart =[...cart, country];
+    setCart(newCart);
+
+  };
   return (
     <div className="App">
+
+<h1>country loaded:{countries.length}</h1>
+<h4>country added:{cart.length}</h4>
+<Cart cart={cart}></Cart>
+{
+  countries.map(country =><Country country={country} handleAddCountry={handleAddCountry} key={country.alpha3Code}></Country>)
+}
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
